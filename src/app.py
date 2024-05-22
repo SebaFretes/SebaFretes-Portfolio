99dd9790cbca166ef71e65fe45f5462d53dcfbb4
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-22 19:27:45 +0000

    contact form done and working

--- src/app.py
+++ src/app.py
@@ -10,13 +10,61 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
+from flask_mail import Mail, Message
+from flask_cors import CORS, cross_origin
 # from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
+CORS(app)
+
+app.config['MAIL_SERVER']= 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.getenv("SMTP_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("SMTP_PASSWORD")
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+@app.route('/sendemail', methods=['POST'])
+def send_email():
+    # Obtener los datos del cuerpo de la solicitud
+    data = request.json
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    # Construir el correo electrónico personalizado
+    sender = os.getenv("SMTP_USERNAME")
+    msg = Message(
+        subject='Mensaje de {}'.format(name),
+        sender=sender,
+        recipients=['sebafretesca@gmail.com']
+    )
+    html_body =f'''
+            <!DOCTYPE html>
+            <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <title>Correo electrónico HTML</title>
+            </head>
+            <body style="font-family: Arial, sans-serif;">
+                <h1>Mensaje enviado desde mi Portfolio</h1>
+                <p style="margin-bottom: 10px;">Mensaje de {name} con email {email}</p>
+                <p>{message}</p>
+            </body>
+            </html>
+    '''
+    msg.html = html_body
+    
+    # Enviar el correo electrónico
+    mail.send(msg)
+    
+    return "Message sent!", 200
+
 app.url_map.strict_slashes = False
 
 # database condiguration
--- src/front/js/pages/contact.js
+++ src/front/js/pages/contact.js
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    }
+
+    const emailFetch = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(process.env.BACKEND_URL + "/sendemail", {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    message: message
+                })
+            })
+
+            if (response.ok) {
+                alert('Message sent successfully');
+                setName('');
+                setEmail('');
+                setMessage('');
+            }
+            else {
+                throw new Error('Error sending the message');
+            }
+        }
+        catch (error) {
+            console.log(error);
+            alert('Error sending the message');
+        }
+    }
+
     return (
         <div className="container">
-
-
             <div style={{ width: '100%' }}> </div>
-
             {/* backgroundColor: '#252525' */}
-            <form style={{ margin: '1rem auto', backgroundColor: '#252525', width: '90%', padding: '2rem', justifyContent: 'center', alignItems: 'center', maxWidth: '50rem', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', boxSizing: 'border-box', color: '#000' }}>
+            <form onSubmit={emailFetch} style={{ margin: '1rem auto', backgroundColor: '#252525', width: '90%', padding: '2rem', justifyContent: 'center', alignItems: 'center', maxWidth: '50rem', borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', boxSizing: 'border-box', color: '#000' }}>
 
                 <h3 style={{ textAlign: 'center', marginBottom: '0.5rem', color: '#000' }}>CONTACT</h3>
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="nombre">NAME</label>
+                    <label htmlFor="name">NAME</label>
                     <input
                         type="text"
-                        name="nombre"
-                        // value={nombre}
-                        // onChange={handleNameChange}
+                        name="name"
+                        value={name}
+                        onChange={handleNameChange}
                         style={{ width: '100%', padding: '0.5rem', boxSizing: 'border-box' }}
                     />
                 </div>
@@ -27,19 +71,19 @@ export const Contact = () => {
                     <input
                         type="email"
                         name="email"
-                        // value={email}
-                        // onChange={handleEmailChange}
+                        value={email}
+                        onChange={handleEmailChange}
                         style={{ width: '100%', padding: '0.5rem', boxSizing: 'border-box' }}
                     />
                 </div>
 
                 <div style={{ marginBottom: '1rem' }}>
-                    <label htmlFor="mensaje">MESSAGE</label>
+                    <label htmlFor="message">MESSAGE</label>
                     <textarea
                         rows="6"
-                        name="mensaje"
-                        // value={mensaje}
-                        // onChange={handleMensajeChange}
+                        name="message"
+                        value={message}
+                        onChange={handleMessageChange}
                         style={{ width: '100%', padding: '0.5rem', boxSizing: 'border-box' }}
                     ></textarea>
                 </div>
