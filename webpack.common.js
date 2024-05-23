7b50f0250cc130bfe8f95ac70aad0db3606ee355
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-23 22:32:35 +0000

    favicon commented

--- webpack.common.js
+++ webpack.common.js
@@ -65,7 +65,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: '4geeks.ico',
+      // favicon: '4geeks.ico',
       template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
