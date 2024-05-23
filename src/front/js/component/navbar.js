6299f1accc12ee5b07fb57944c18285c69560cde
Seba Fretes <84410616+SebaFretes@users.noreply.github.com>
2024-05-23 05:36:11 +0000

    making the navbar responsive

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,26 +3,33 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-md navbar-light bg-light">
 			<div className="container">
-				<Link to="/">
+				<Link to="/" className="navbar-brand">
 					<button className="btn" style={{ fontSize: '1.4rem', fontWeight: '700', backgroundColor: '#F9F9F9' }}>SEBASTIAN FRETES</button>
 				</Link>
-				<div className="ml-auto ">
-					<Link to="/info">
-						<button className="btn-grey">ABOUT</button>
-					</Link>
-					<Link to="/projects">
-						<button className="btn-grey">PROJECTS</button>
-					</Link>
-					<Link to="/certifications">
-						<button className="btn-grey">CERTIFICATIONS</button>
-					</Link>
-					<Link to="/contact">
-						<button className="btn-grey">CONTACT</button>
-					</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<div className="navbar-nav ms-auto">
+						<Link to="/info" className="nav-item">
+							<button className="btn-grey">ABOUT</button>
+						</Link>
+						<Link to="/projects" className="nav-item">
+							<button className="btn-grey">PROJECTS</button>
+						</Link>
+						<Link to="/certifications" className="nav-item">
+							<button className="btn-grey">CERTIFICATIONS</button>
+						</Link>
+						<Link to="/contact" className="nav-item">
+							<button className="btn-grey">CONTACT</button>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</nav>
 	);
 };
+
+
